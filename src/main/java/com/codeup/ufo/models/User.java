4c99f34abbb4bb8f456c4d7dc486af597e2ada6a
Justin McCaleb <justinleemccaleb@gmail.com>
2020-09-03 12:10:47 -0500

    Feat: add user model w/o annotations

---
+++ src/main/java/com/codeup/ufo/models/User.java
@@ -0,0 +1,57 @@
+package com.codeup.ufo.models;
+
+
+public class User {
+
+    //Properties
+    private long id;
+
+    private String username;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String password;
+
+    //Constructor
+    public User() {
+    }
+
+    //Getters and Setters
+    public long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
