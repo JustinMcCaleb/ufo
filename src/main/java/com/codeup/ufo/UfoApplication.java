18da30be812a40febc10cf86204b6367fb954b5b
Justin McCaleb <justinleemccaleb@gmail.com>
2020-09-03 11:18:12 -0500

    first commit

---
+++ src/main/java/com/codeup/ufo/UfoApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.ufo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UfoApplication.class, args);
+    }
+
+}
